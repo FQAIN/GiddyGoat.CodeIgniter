fec8e49215d8439148fe910b246a72c37112bd8b
Fyaz Qadir Ahmed Ikram <54445998+FQAIN@users.noreply.github.com>
2021-01-18 18:05:02 +0000

    encryption set to 1

--- application/config/config.php
+++ application/config/config.php
@@ -317,7 +317,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/userguide3/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '1';
 
 /*
 |--------------------------------------------------------------------------
